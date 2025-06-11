98cab87a2d77ee8180fd49a1cab1d34f40a95d68
otetoteto <oteto2oteto@gmail.com>
2025-06-11 22:03:36 +0900

    指定できるタグの制御

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 // 1. ユーティリティを`astro:content`からインポート
 import { defineCollection, z } from "astro:content";
 
+const tags = [z.literal("ひとりごと"), z.literal("Astro"), z.literal("React")] as const;
+
 // 2. 各コレクションに`type`と`schema`を定義
 const blogCollection = defineCollection({
   type: "content", // v2.5.0以降
@@ -8,7 +10,7 @@ const blogCollection = defineCollection({
     title: z.string(),
     description: z.string(),
     author: z.string(),
-    tags: z.array(z.string()),
+    tags: z.array(z.union(tags)).min(1, "タグを1つ以上指定してください"),
     published: z.boolean(),
     publishedDate: z.string(),
   }),
