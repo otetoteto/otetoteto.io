667270368fd7ffddc8f320a27fa18d2b340df80e
otetoteto <oteto2oteto@gmail.com>
2025-07-03 00:06:11 +0900

    tailwind導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://otetoteto.io",
   integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   devToolbar: {
     enabled: false,
-  },
-  vite: {
-    css: {
-      transformer: "lightningcss",
-    },
   },
   markdown: {
     shikiConfig: {
